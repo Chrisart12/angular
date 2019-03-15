7add6c913ff183c8e2aa2b3b571414503b51603d
chris <chrisissifou@gmail.com>
2019-03-15 23:44:59 +0100

    J'ai généré le component mon-premier

---
+++ my-first-project/src/app/mon-premier/mon-premier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-premier',
+  templateUrl: './mon-premier.component.html',
+  styles: []
+})
+export class MonPremierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
